912f2f18128afb22a722d34645e92824bb1095ff
Daniil Detter <vudi600@gmail.com>
2023-04-19 23:12:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
     <title>Лабораторные работы</title>
 </head>
 <body>
@@ -15,6 +18,13 @@
         <h3>
             Выполнил: Деттер Даниил Николаевич
         </h3>
+        <div class="social">
+                <a href="https://vk.com/dt.danil"><img src="assets/icons/vk.png"></a>
+                <a href="https://t.me/tarodictrl"><img src="assets/icons/telegram.png"></a>
+                <a href="https://github.com/Tarodictrl"><img src="assets/icons/github.png"></a>
+                <a href="https://steamcommunity.com/id/tarodictrl/"><img src="assets/icons/steam.png"></a>
+                <a href="mailto:vudi600@gmail.com"><img src="assets/icons/email.png"></a>
+        </div>
     </div>
     <div class="menu">
         <a href="lab1/lab1.php" class="select">Лабораторная работа №1</a>
