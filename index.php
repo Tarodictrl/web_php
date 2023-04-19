8e1777496909827a0252ec3b547ac975e37b4c99
Daniil Detter <vudi600@gmail.com>
2023-04-19 22:30:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Лабораторные работы</title>
+</head>
+<body>
+    <div class="title">
+        <h1>
+            Лабораторные работы
+        </h1>
+        <h3>
+            Выполнил: Деттер Даниил Николаевич
+        </h3>
+    </div>
+    <div class="menu">
+        <a href="lab1/lab1.php">Лабораторная работа №1</a>
+    </div>
+</body>
+</html>
