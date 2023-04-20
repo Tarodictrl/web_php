6686b2353e02bb2fa5850efe092ba70c137eda1d
Daniil Detter <vudi600@gmail.com>
2023-04-20 14:11:08 +0700

    Update

--- lab4/lab4.php
+++ lab4/lab4.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../index.css">
     <title>Лабораторная работа №4</title>
 </head>
 <body>
-    <h1>
-        Лабораторная работа №4
-    </h1>
+    <div class="title">
+        <h1>
+            Лабораторная работа №4
+        </h1>
+    </div>
 </body>
 </html>
 
--- lab7-8/lab7-8.php
+++ lab7-8/lab7-8.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../index.css">
     <title>Лабораторная работа №7-8</title>
 </head>
 <body>
-    <h1>
-        Лабораторная работа №7-8
-    </h1>
+    <div class="title">
+        <h1>
+            Лабораторная работа №7-8
+        </h1>
+    </div>
+    <div class="menu">
+        <a href="task1/task1.php" class="select">Задача 1</a>
+        <a href="task2/task2.php" class="select">Задача 2</a>
+        <a href="task3/task3.php" class="select">Задача 3</a>
+    </div>
 </body>
 </html>
 
